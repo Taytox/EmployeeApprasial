75b297b62ee46b3f8b9b381e1a53ca544c6696ea
Taytox <50372271+Taytox@users.noreply.github.com>
2022-06-03 17:31:44 +0100

    Added Demo Commit comment for assessment.

--- src/main/java/com/employeeappraisal/EmployeeappraisalApplication.java
+++ src/main/java/com/employeeappraisal/EmployeeappraisalApplication.java
@@ -18,6 +18,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+//Comment for Demo Commit
+
 @SpringBootApplication
 public class EmployeeappraisalApplication {
       private static final Logger log = LoggerFactory.getLogger(EmployeeappraisalApplication.class);
